cc63a82102a8533c504d7d824d7670fcd5c70eba
Chris Humphrey <relfen@gmail.com>
2025-09-26 09:54:33 -0700

    fix: Exclude cardContainer and card widgets from ID requirement check
    - Add 'cardContainer' and 'card' to WIDGET_TYPES_WITHOUT_ID_REQUIREMENT
    - Cards don't use IDs, so both cardContainer and card widgets are excluded

--- parser/rules/structure/widgets/widget_id_required.py
+++ parser/rules/structure/widgets/widget_id_required.py
@@ -19,7 +19,7 @@ class WidgetIdRequiredRule(Rule):
     
     # Widget types that do not require or support ID values
     WIDGET_TYPES_WITHOUT_ID_REQUIREMENT = {
-        'footer', 'item', 'group', 'title', 'pod'
+        'footer', 'item', 'group', 'title', 'pod', 'cardContainer', 'card'
     }
 
     def analyze(self, context):
